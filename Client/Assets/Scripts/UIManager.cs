0a4181bff8362f7429f47dbfa75757b2d903eabe
oguzhanaydemir <ouzhanaydemir7@gmail.com>
2017-04-19 15:10:00 +0300

    Add UIManager script. Find some panel references.

--- Client/Assets/Scripts/UIManager.cs
+++ Client/Assets/Scripts/UIManager.cs
@@ -1,15 +1,45 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class UIManager : MonoBehaviour {
 
-	// Use this for initialization
+    private List<GameObject> Panels = new List<GameObject>();
+
+    #region PANELS
+        public GameObject PNL_WELCOME;
+        public GameObject PNL_GUIDE;
+        public GameObject PNL_QUESTIONS;
+    #endregion
+
+    void FindPanelsGameObjects()
+    {
+        PNL_WELCOME   = transform.FindChild("pnlWelcome").gameObject;
+        PNL_GUIDE     = transform.FindChild("pnlGuide").gameObject;
+        PNL_QUESTIONS = transform.FindChild("pnlQuestions").gameObject;
+
+        Panels.Add(PNL_WELCOME);
+        Panels.Add(PNL_GUIDE);
+        Panels.Add(PNL_QUESTIONS);
+    }
+
+    void OpenScreen(GameObject screen)
+    {
+        foreach (GameObject item in Panels)
+        {
+            if (item == screen)
+                item.SetActive(true);
+            else
+                item.SetActive(false);
+        }
+    }
+
 	void Start () {
-		
+        FindPanelsGameObjects(); //Initialize all panel objects references.
+        OpenScreen(PNL_WELCOME);
 	}
 	
-	// Update is called once per frame
 	void Update () {
 		
 	}
