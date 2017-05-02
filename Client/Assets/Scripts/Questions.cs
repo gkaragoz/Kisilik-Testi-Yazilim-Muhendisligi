611f568fc62be00f05e96cbcb4aa6e27f6bf1893
gkaragoz <annotto3@gmail.com>
2017-05-02 21:14:43 +0300

    Read JSON file.

--- Client/Assets/Scripts/Questions.cs
+++ Client/Assets/Scripts/Questions.cs
@@ -4,13 +4,13 @@ using UnityEngine;
 
 public class Questions : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    private JSONObject data;
+
+    void Start()
+    {
+        string dataPath = (Resources.Load<TextAsset>("Data") as TextAsset).text;
+
+        data = new JSONObject(dataPath);
+        Debug.Log(data);
+    }
 }
