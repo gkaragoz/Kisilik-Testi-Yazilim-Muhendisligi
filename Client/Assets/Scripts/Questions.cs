737e894af895633ff1f82cae84209182633b9056
gkaragoz <annotto3@gmail.com>
2017-05-03 00:31:39 +0300

    Fix JSON data. Encoding stuff.

--- Client/Assets/Scripts/Questions.cs
+++ Client/Assets/Scripts/Questions.cs
@@ -1,6 +1,9 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using System.IO;
+using UnityEditor;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Questions : MonoBehaviour {
 
@@ -11,6 +14,6 @@ public class Questions : MonoBehaviour {
         string dataPath = (Resources.Load<TextAsset>("Data") as TextAsset).text;
 
         data = new JSONObject(dataPath);
-        Debug.Log(data);
+        Debug.Log(data.GetField("Questions")[0].GetField("Question"));
     }
 }
